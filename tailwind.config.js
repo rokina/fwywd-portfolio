b7c3b1e95d751fe0dc1847c5fd8e923da35c7bb4
Hiroki Sato <raxito2008@yahoo.co.jp>
2021-07-30 19:05:49 +0900

    add tailwindcss インストール 初期設定

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      'text-black': '#384359',
+      'text-green': '#008c8d',
+      'bg-green': '#6bc2c3',
+      'bg-black': '#262c3a',
+      'progress-pale': '#c5eaea',
+      'progress-deep': '#2bb9ba',
+      'border-green': '#c5eaea',
+    },
+    container: {
+      center: true,
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
