e2103927df0b3c17b353fff32f8d0e87140ff7e7
hk-sato <hk-sato@respect-pal.jp>
2021-08-03 15:40:41 +0900

    update ヘッダーコンポーネント作成

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,8 @@
 import 'styles/globals.css'
-import { Header } from 'components/Index';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Header />
       <Component {...pageProps} />
     </>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
+import { Header } from 'components/Index';
+
 const Home = () => {
-	return (
-		<div>
-		</div>
-	)
+  return (
+    <Header />
+  )
 }
 
 export default Home
